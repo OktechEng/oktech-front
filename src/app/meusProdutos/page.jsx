8e3d6394536dc625612113bce205a25d361866c0
Yasmim <barrosyasmim13@gmail.com>
2025-08-08 16:38:31 -0300

    Add MeusProdutosTemplate component and page for product display

---
+++ src/app/meusProdutos/page.jsx
@@ -0,0 +1,13 @@
+// import MeusProdutosTemplate from "@/components/templates/MeusProdutos/MeusProdutosTemplate";
+import MeusProdutosTemplate from "@/template/MeusProdutos/MeusProdutosTemplate";
+
+export const metadata = {
+  title: "Meus Produtos",
+  };
+
+export default function Page() {
+    return (
+        <MeusProdutosTemplate />
+        
+    );
+    }
